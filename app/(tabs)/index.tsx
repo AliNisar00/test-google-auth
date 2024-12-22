bbdc5e4788c6f19a34f12d52eba4a3710c9f547a
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-12-22 23:18:37 +0500

    Added functionality to store user sign in information using async storage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,8 +10,43 @@ export default function HomeScreen() {
     androidClientId: "858914503669-a6hbrj0d3hvc8iom8dsdhqt2la9vt3c7.apps.googleusercontent.com",
     webClientId: "858914503669-68sagpn0erv4a3rvs1t61bvoa03ke2hu.apps.googleusercontent.com",
   });
+
+  React.useEffect(() => {
+    handleSignInWithGoogle();
+  }, [response])
+
+  async function handleSignInWithGoogle() {
+    const user = await AsyncStorage.getItem("@user");
+    if (!user) {
+      if (response?.type === "success") {
+        await getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(JSON.parse(user));
+    }
+  }
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // To add error handler here
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <Text>{JSON.stringify(userInfo)}</Text>
       <Text>Hello world!</Text>
       <Button title="Sign in with Google" onPress={promptAsync} />
     </View>
