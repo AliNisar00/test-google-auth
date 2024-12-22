e9d896e9e08caa74e36be2ca8c0b271fbbcf67b6
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-12-22 22:47:58 +0500

    Made preparations for Google OAuth authorization setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,6 @@
 // web 858914503669-68sagpn0erv4a3rvs1t61bvoa03ke2hu.apps.googleusercontent.com
-//android 
+//android 858914503669-a6hbrj0d3hvc8iom8dsdhqt2la9vt3c7.apps.googleusercontent.com
 import { Image, StyleSheet, Platform, View, Text } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
